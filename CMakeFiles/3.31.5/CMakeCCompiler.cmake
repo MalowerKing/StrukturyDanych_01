02e89595e40b14e9e13e265f6eb47fbf1e8940a4
KingMalower <maciejzych2003@gmail.com>
2025-04-18 14:08:10 +0200

    Cmake testy

---
+++ CMakeFiles/3.31.5/CMakeCCompiler.cmake
@@ -0,0 +1,81 @@
+set(CMAKE_C_COMPILER "/nix/store/8v6k283dpbc0qkdq81nb6mrxrgcb10i1-gcc-wrapper-14-20241116/bin/gcc")
+set(CMAKE_C_COMPILER_ARG1 "")
+set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_VERSION "14.2.1")
+set(CMAKE_C_COMPILER_VERSION_INTERNAL "")
+set(CMAKE_C_COMPILER_WRAPPER "")
+set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "17")
+set(CMAKE_C_EXTENSIONS_COMPUTED_DEFAULT "ON")
+set(CMAKE_C_STANDARD_LATEST "23")
+set(CMAKE_C_COMPILE_FEATURES "c_std_90;c_function_prototypes;c_std_99;c_restrict;c_variadic_macros;c_std_11;c_static_assert;c_std_17;c_std_23")
+set(CMAKE_C90_COMPILE_FEATURES "c_std_90;c_function_prototypes")
+set(CMAKE_C99_COMPILE_FEATURES "c_std_99;c_restrict;c_variadic_macros")
+set(CMAKE_C11_COMPILE_FEATURES "c_std_11;c_static_assert")
+set(CMAKE_C17_COMPILE_FEATURES "c_std_17")
+set(CMAKE_C23_COMPILE_FEATURES "c_std_23")
+
+set(CMAKE_C_PLATFORM_ID "Linux")
+set(CMAKE_C_SIMULATE_ID "")
+set(CMAKE_C_COMPILER_FRONTEND_VARIANT "GNU")
+set(CMAKE_C_SIMULATE_VERSION "")
+
+
+
+
+set(CMAKE_AR "/nix/store/8v6k283dpbc0qkdq81nb6mrxrgcb10i1-gcc-wrapper-14-20241116/bin/ar")
+set(CMAKE_C_COMPILER_AR "/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/bin/gcc-ar")
+set(CMAKE_RANLIB "/nix/store/8v6k283dpbc0qkdq81nb6mrxrgcb10i1-gcc-wrapper-14-20241116/bin/ranlib")
+set(CMAKE_C_COMPILER_RANLIB "/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/bin/gcc-ranlib")
+set(CMAKE_LINKER "/nix/store/8v6k283dpbc0qkdq81nb6mrxrgcb10i1-gcc-wrapper-14-20241116/bin/ld")
+set(CMAKE_LINKER_LINK "")
+set(CMAKE_LINKER_LLD "")
+set(CMAKE_C_COMPILER_LINKER "/nix/store/r8gajll0yb0c08hd734lyj3jmqav3p9a-binutils-wrapper-2.43.1/bin/ld")
+set(CMAKE_C_COMPILER_LINKER_ID "GNU")
+set(CMAKE_C_COMPILER_LINKER_VERSION 2.43.1)
+set(CMAKE_C_COMPILER_LINKER_FRONTEND_VARIANT GNU)
+set(CMAKE_MT "")
+set(CMAKE_TAPI "CMAKE_TAPI-NOTFOUND")
+set(CMAKE_COMPILER_IS_GNUCC 1)
+set(CMAKE_C_COMPILER_LOADED 1)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_C_ABI_COMPILED TRUE)
+
+set(CMAKE_C_COMPILER_ENV_VAR "CC")
+
+set(CMAKE_C_COMPILER_ID_RUN 1)
+set(CMAKE_C_SOURCE_FILE_EXTENSIONS c;m)
+set(CMAKE_C_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_C_LINKER_PREFERENCE 10)
+set(CMAKE_C_LINKER_DEPFILE_SUPPORTED )
+
+# Save compiler ABI information.
+set(CMAKE_C_SIZEOF_DATA_PTR "8")
+set(CMAKE_C_COMPILER_ABI "ELF")
+set(CMAKE_C_BYTE_ORDER "LITTLE_ENDIAN")
+set(CMAKE_C_LIBRARY_ARCHITECTURE "")
+
+if(CMAKE_C_SIZEOF_DATA_PTR)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_C_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_C_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_C_COMPILER_ABI}")
+endif()
+
+if(CMAKE_C_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "")
+endif()
+
+set(CMAKE_C_CL_SHOWINCLUDES_PREFIX "")
+if(CMAKE_C_CL_SHOWINCLUDES_PREFIX)
+  set(CMAKE_CL_SHOWINCLUDES_PREFIX "${CMAKE_C_CL_SHOWINCLUDES_PREFIX}")
+endif()
+
+
+
+
+
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "/nix/store/f2krmq3iv5nibcvn4rw7nrnrciqprdkh-python3-3.12.9/include;/nix/store/qlwaaawwhhf5mh61xpgj1apxgjrxcalf-gtest-1.16.0-dev/include;/nix/store/xylg7c2w19n7lpvc7xs3zgkh5is934lv-mesa-25.0.3/include;/nix/store/s38jf0la3gdx1v2gr6317fjc2czlhf2q-libglvnd-1.7.0-dev/include;/nix/store/4lk7dvwsii9dcp11bxqbigj6549avnqy-libX11-1.8.12-dev/include;/nix/store/i8vqy9y74dxnxyc9636db5kxmbs0ckvf-xorgproto-2024.1/include;/nix/store/0c6hy7ih8hgx070k0jric3rabwc68nj6-libxcb-1.17.0-dev/include;/nix/store/611bplx5gqw9kjb7ppdpar3la9kmk9s3-libXft-2.3.8-dev/include;/nix/store/cz5gva0jndn2273p2b23ad3n34rwgfha-libXrender-0.9.12-dev/include;/nix/store/2dgzl64w68mglcn6mi82rnvk37df33qz-freetype-2.13.3-dev/include;/nix/store/h1ryid6kgb2s8nbghn4wg750z5jl27nk-zlib-1.3.1-dev/include;/nix/store/la7r5fbm42lphf71znlk900mj25x57m6-bzip2-1.0.8-dev/include;/nix/store/3yscaaha27gnrvch82lck0a11wjr1z3y-brotli-1.1.0-dev/include;/nix/store/0z4cz1mfpwbs7k1zikfimaq80hcpn103-libpng-apng-1.6.46-dev/include;/nix/store/nziailpyy0d90vq5pi1kckbxzvjcg5ng-fontconfig-2.16.0-dev/include;/nix/store/s6kn354pxysnv2c9cxasqxbxl10xy7y9-libXinerama-1.1.5-dev/include;/nix/store/7v9lq96hibxf6zyl6nfhsl0y5g77zmcb-glfw-3.4/include;/nix/store/l94yyg86v1dl04cvr426ddgz5jlv8rnl-glew-2.2.0-dev/include;/nix/store/d4w52p15sz627jygwjcyfpz3riy18h1g-glu-9.0.3-dev/include;/nix/store/1wk01bxqgiikqri3bv12aaz69ysws7d3-freeglut-3.6.0-dev/include;/nix/store/mmfmyi6fl8ggvhwd2nfc9ds1d71gkwz6-gdb-16.2/include;/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/lib/gcc/x86_64-unknown-linux-gnu/14.2.1/include;/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/include;/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/lib/gcc/x86_64-unknown-linux-gnu/14.2.1/include-fixed;/nix/store/71xyq87gpb2qrwn314p0sf2n002lgd91-glibc-2.40-66-dev/include")
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "gcc;gcc_s;c;gcc;gcc_s")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/nix/store/f2krmq3iv5nibcvn4rw7nrnrciqprdkh-python3-3.12.9/lib;/nix/store/h2xr1s28nggjx08mkgpv7zvgkkbxypii-gtest-1.16.0/lib;/nix/store/xylg7c2w19n7lpvc7xs3zgkh5is934lv-mesa-25.0.3/lib;/nix/store/ylzpk54f82kc888pr4325ynshk48la6i-libglvnd-1.7.0/lib;/nix/store/nfkkx2iz3injlh130y3b0zgip5981y33-libxcb-1.17.0/lib;/nix/store/wfrgia7qj3ijrxl9irsqvjfz20qqx5fm-libX11-1.8.12/lib;/nix/store/l3138hd3gy3i0gcpw9xs41h6a8hf0m00-libXrender-0.9.12/lib;/nix/store/xijpnhg8mg0g4lahwrxdwylpcq7249gc-zlib-1.3.1/lib;/nix/store/r2cw2amwdjwdfcdrvfbq1cj4ryi6yfs5-bzip2-1.0.8/lib;/nix/store/ka86qsp50hdljs1qmn888yw1s75grxac-brotli-1.1.0-lib/lib;/nix/store/miq99gylww7177pdm7n7ygcryvlnyp70-libpng-apng-1.6.46/lib;/nix/store/2mmsgkggc79x1ayvd4hkbs6k2bqvw091-freetype-2.13.3/lib;/nix/store/pqb8qbd3bq9r7vndbphavjgmvcd8zs66-fontconfig-2.16.0-lib/lib;/nix/store/iv9jfm7scprsvax6kzjr3flm09pak7kc-libXft-2.3.8/lib;/nix/store/344qwdpsgflcv3j2l2nn30r7p9816kpg-libXinerama-1.1.5/lib;/nix/store/7v9lq96hibxf6zyl6nfhsl0y5g77zmcb-glfw-3.4/lib;/nix/store/q6063jl4mgbz9fk8hnfy1l5m8y9vldl5-glu-9.0.3/lib;/nix/store/0biq5934f1sgp3391al4kcanprqaxv3l-glew-2.2.0/lib;/nix/store/4shi5fy3m7bdj9p060mvxyhvfdka4r21-freeglut-3.6.0/lib;/nix/store/mmfmyi6fl8ggvhwd2nfc9ds1d71gkwz6-gdb-16.2/lib;/nix/store/rmy663w9p7xb202rcln4jjzmvivznmz8-glibc-2.40-66/lib;/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/lib/gcc/x86_64-unknown-linux-gnu/14-20241116;/nix/store/ik84lbv5jvjm1xxvdl8mhg52ry3xycvm-gcc-14-20241116-lib/lib;/nix/store/r8gajll0yb0c08hd734lyj3jmqav3p9a-binutils-wrapper-2.43.1/bin;/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/lib/gcc/x86_64-unknown-linux-gnu/14.2.1;/nix/store/2agih0y5ns3sgbviw2xhivdgg59b41g9-gcc-14-20241116/lib")
+set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
---
+++ CMakeFiles/resoult.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resoult.dir/link.d"
+  "CMakeFiles/resoult.dir/src/main.cpp.o"
+  "CMakeFiles/resoult.dir/src/main.cpp.o.d"
+  "resoult"
+  "resoult.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resoult.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
